ba2c002abec968d3c235fddf3f8f0cea5d071697
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-07-11 16:28:49 -0400

    Completed Initial app.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Form from "./components/form";
+
+const initialFormValues = {
+  username: "",
+  email: "",
+  role: "",
+};
+
+const teamMembers = [
+  {
+    username: "shariq",
+    email: "shariq@lambda.com",
+    role: "engineer",
+  },
+];
 
 function App() {
+  const [formValues, setformValues] = useState(initialFormValues);
+  const [members, setMembers] = useState(teamMembers);
+
+  const update = (event) => {};
+  const submit = (event) => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {" "}
+      <h1>Team Builder</h1>
     </div>
   );
 }
---
+++ team-builder/src/components/form.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+export default function Form(props) {
+  const { teamMembers } = props;
+  console.log(teamMembers);
+
+  return <div>Test</div>;
+}
